c1641f627b23edc578930cafd217b37db17c682c
gavin <gavin_dtl@163.com>
2019-02-25 17:42:50 +0800

    修改KEY文件

--- EasyScreenLive-Android/rtspservice/src/main/java/org/easydarwin/rtspservice/JniEasyScreenLive.java
+++ EasyScreenLive-Android/rtspservice/src/main/java/org/easydarwin/rtspservice/JniEasyScreenLive.java
@@ -132,7 +132,7 @@ public class JniEasyScreenLive {
     }
 
     public int active(Context context){
-        String key = "6D72754B7A4A36526D3430414D484E636F347A5070654A76636D63755A57467A65575268636E64706269356C59584E3563324E795A57567562476C325A56634D5671442F7065424859585A7062695A4359574A76633246414D6A41784E6B566863336C4559584A33615735555A5746745A57467A65513D3D";
+        String key = Config.KEY;
         int iRet = active(key, context);
         if(iRet != 0){
             Log.e(TAG, "active libEasyIPCamera failed!!! return : " + iRet);
